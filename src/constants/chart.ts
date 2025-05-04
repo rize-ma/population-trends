5f27ad8caa177ae36ecd8d9011c14eae96ffbe43
rize-ma <masaki030603@gmail.com>
2025-05-05 03:08:48 +0900

    feat: 人口推移グラフ用データ作成

---
+++ src/constants/chart.ts
@@ -0,0 +1,18 @@
+import type { MantineColor } from '@mantine/core';
+
+export const CHART_COLORS: MantineColor[] = [
+  'green',
+  'red',
+  'blue',
+  'pink',
+  'grape',
+  'violet',
+  'indigo',
+  'cyan',
+  'lime',
+  'yellow',
+  'gray',
+  'orange',
+  'teal',
+  'dark',
+] as const;
