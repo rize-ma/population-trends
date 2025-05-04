06ba65c683d0cd3bfbeb9eb5cdc788e231eef2ee
rize-ma <masaki030603@gmail.com>
2025-05-04 23:52:12 +0900

    test: prefectures apiのキャッシュを使用した場合のテストケース追加

--- src/api/prefecture/prefecture.ts
+++ src/api/prefecture/prefecture.ts
@@ -1,7 +1,11 @@
+import type { FetchOptions } from '../../types/api';
 import type { FetchPrefecturesResponse, PrefectureResult } from '../../types/prefecture';
 import { fetchApi } from '../fetchApi';
 
-export const fetchPrefectures = async (): Promise<PrefectureResult> => {
-  const res = await fetchApi<FetchPrefecturesResponse>('/api/v1/prefectures');
+export const fetchPrefectures = async (options: FetchOptions = {}): Promise<PrefectureResult> => {
+  const res = await fetchApi<FetchPrefecturesResponse>('/api/v1/prefectures', {
+    ...options,
+    method: 'GET',
+  });
   return res.result;
 };
